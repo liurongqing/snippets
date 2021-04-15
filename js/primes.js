154324114a823798a45def5feead2723a7325ab7
liurongqing <phpgege@gmail.com>
2021-04-15 14:57:16 +0800

    1

--- js/primes.js
+++ js/primes.js
@@ -8,14 +8,11 @@
  */
 const primes = (num) => {
   let arr = Array.from({ length: num - 1 }).map((_, i) => i + 2)
-  // let sqroot = Math.floor(Math.sqrt(num))
-  // let numsTillSqroot = Array.from({ length: sqroot - 1 }).map((_, i) => i + 2)
+  let sqroot = Math.floor(Math.sqrt(num))
+  let numsTillSqroot = Array.from({ length: sqroot - 1 }).map((_, i) => i + 2)
 
-  // numsTillSqroot.forEach(
-  //   (x) => (arr = arr.filter((y) => y % x !== 0 || y === x))
-  // )
+  numsTillSqroot.forEach(
+    (x) => (arr = arr.filter((y) => y % x !== 0 || y === x))
+  )
   return arr
 }
-
-const a = primes(10)
-console.log(a)
