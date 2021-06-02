4072f0e833f19552db4e08109fb65831d1c4388b
liurongqing <phpgege@gmail.com>
2021-06-02 15:20:01 +0800

    1

--- code/nest/nest.js
+++ code/nest/nest.js
@@ -1,14 +1,4 @@
-树状递归
-
-## 图片
-
-![nest](./nest.png)
-
-## 源码复制
-
-```js
 const nest = (items, id = null, link = 'parent_id') =>
   items
     .filter((item) => item[link] === id)
     .map((item) => ({ ...item, children: nest(items, item.id, link) }))
-```
